ed97582b0c971138f04927f6eddc652e01ddce86
jeremyindie <59747752+jeremyindie@users.noreply.github.com>
2021-05-14 08:46:08 -0400

    Add files via upload

---
+++ HorrorStory/Assets/Scripts/Location/SetArea.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetArea : MonoBehaviour
+{
+
+    public LocationController.Location location;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ////if player
+        if (location != LocationController.currentLocation)
+        {
+            LocationController.previousLocation = LocationController.currentLocation;
+            LocationController.currentLocation = location;
+        }
+    }
+}
