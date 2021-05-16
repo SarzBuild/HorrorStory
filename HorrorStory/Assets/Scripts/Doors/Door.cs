ed7157dc0b01aeb7f548c9aea9eb416cc92baa7d
jeremyindie <59747752+jeremyindie@users.noreply.github.com>
2021-05-16 12:57:22 -0400

    Update Door.cs

--- HorrorStory/Assets/Scripts/Doors/Door.cs
+++ HorrorStory/Assets/Scripts/Doors/Door.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityCore.Audio;
 
 public class Door : Interactable
 {
@@ -9,11 +10,29 @@ public class Door : Interactable
     [SerializeField] DoorCol enterCol;
     Animator myAnim;
     public bool canBeOpened = true;
+    public UnityCore.Audio.AudioType lockedDoorAudio;
+    public UnityCore.Audio.AudioType openDoor;
+    public UnityCore.Audio.AudioType closeDoor;
+    public UnityCore.Audio.AudioType unlockDoor;
+
+
+    public enum TriggerTypes { none, copy, next, finish }
+    public TriggerTypes triggerType = TriggerTypes.none;
+    private Transform initialTransform;
+    private bool roomIsSpawned = false;
+
+
     private bool isOpening = false;
     private void Awake()
     {
         myAnim = GetComponentInParent<Animator>();
+        roomIsSpawned = false;
+        initialTransform = transform;
+
     }
+
+
+
     private void OpenDoor()
     {
         if (canBeOpened)
@@ -23,10 +42,37 @@ public class Door : Interactable
             myAnim.SetBool("isClosing", false);
             canBeOpened = false;
             isOpening = true;
+            AudioAction.PlaySound(openDoor);
+            switch (triggerType)
+            {
+                case (TriggerTypes.none):
+                    break;
+                case (TriggerTypes.copy):
+                    if (roomIsSpawned) return;
+                    RoomAction.spawnCopyRoom(initialTransform);
+                    roomIsSpawned = true;
+                    break;
+                case (TriggerTypes.next):
+                    if (roomIsSpawned) return;
+                    RoomAction.spawnNextRoom(initialTransform);
+                    roomIsSpawned = true; 
+                    break;
+                case (TriggerTypes.finish):
+                    if (roomIsSpawned) return;
+                    RoomAction.finishArea(initialTransform);
+                    roomIsSpawned = true; 
+                    break;
+                default:
+                    break;
+            }
+        }
+        else {
+            AudioAction.PlaySound(lockedDoorAudio);
         }
     }
     public void CloseDoor()
-    {        
+    {
+        AudioAction.PlaySound(closeDoor);
         myAnim.SetBool("isOpening", false);
         myAnim.SetBool("isClosing", true);            
         canBeOpened = true;
@@ -43,6 +89,13 @@ public class Door : Interactable
             return null;
         }
         
+    }
+
+    public void UnlockDoor()
+    {
+        AudioAction.PlaySound(unlockDoor);
+        canBeOpened = true;
+
     }
 
     //If there's an interaction, we call our functions
