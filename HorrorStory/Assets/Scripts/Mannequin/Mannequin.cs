d3e7b1b15e730a0b77ce7aed22931d51295bc9dd
jeremyindie <59747752+jeremyindie@users.noreply.github.com>
2021-05-16 15:26:51 -0400

    Add files via upload

---
+++ HorrorStory/Assets/Scripts/Mannequin/Mannequin.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityCore.Audio;
+
+public class Mannequin : MonoBehaviour
+{
+
+    private Transform goal;
+    private NavMeshAgent agent;
+    private float timeToNextSoundEffect; 
+    private float timeBetweenSoundEffects = 4.0f;
+
+    public UnityCore.Audio.AudioType[] soundEffects; 
+    void Start()
+    {
+
+        agent = GetComponent<NavMeshAgent>();
+        goal = GameObject.FindGameObjectWithTag("Player").transform;
+        AudioAction.StartMannequinSound();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //player dies 
+        AudioAction.PlaySound(UnityCore.Audio.AudioType.GhostCatchesPlayer);
+        timeToNextSoundEffect = 10000f;
+    }
+    void Update()
+    {
+        if(timeToNextSoundEffect < 0)
+        {
+            timeToNextSoundEffect = timeBetweenSoundEffects;
+            int nextSoundEffect = (int)Mathf.Floor(Random.Range(0.0f, 3.99f));
+            AudioAction.PlaySound(soundEffects[nextSoundEffect]);
+        } else
+        {
+            timeToNextSoundEffect -= Time.deltaTime;
+        }
+
+        agent.SetDestination(goal.position);
+
+    }
+}
+
