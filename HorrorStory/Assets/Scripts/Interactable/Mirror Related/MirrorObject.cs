9993f5bc046c1952e6ff9d14134e6244d9c8ca81
jeremyindie <59747752+jeremyindie@users.noreply.github.com>
2021-05-17 20:21:28 -0400

    Update MirrorObject.cs

--- HorrorStory/Assets/Scripts/Interactable/Mirror Related/MirrorObject.cs
+++ HorrorStory/Assets/Scripts/Interactable/Mirror Related/MirrorObject.cs
@@ -7,10 +7,12 @@ public class MirrorObject : Interactable
     //We get the player references to update some values
     private Sc_PlayerReferences playerReferences;
     [SerializeField] private bool invisible;
-    public UnityCore.Audio.AudioType portraitSound; 
+    public UnityCore.Audio.AudioType portraitSound;
+    private float timeSinceAudio = 4.0f; 
     private void Start()
     {
         playerReferences = Sc_PlayerReferences.Instance;
+        timeSinceAudio = 4.0f;
     }
 
     void BreakMirror()
@@ -18,14 +20,19 @@ public class MirrorObject : Interactable
         Destroy(gameObject);
     }
 
-    public void OnLook()
+    public void Update()
     {
-        UnityCore.Audio.AudioAction.PlaySound(portraitSound);
+        timeSinceAudio += Time.deltaTime;
     }
 
     //We return the string that's used when the player's vision is on the game object
     public override string GetDescription()
     {
+        if (timeSinceAudio >= 4.0f)
+        {
+            UnityCore.Audio.AudioAction.PlaySound(portraitSound);
+            timeSinceAudio = 0.0f;
+        }
         return "Left Click to destroy the mirror";
     }
     
